4311d108936c94ca1cadf5d3dd5fe06fcf231de2
Konstantin Shibkov <shibkov.k@gmail.com>
2021-11-08 21:26:58 +0500

    Initial commit

---
+++ src/main/java/practice/Periods.java
@@ -0,0 +1,12 @@
+package practice;
+
+import java.time.LocalDate;
+
+public class Periods {
+
+    // реализуйте вывод разницы между датами, используя класс Period
+    public static String getPeriodFromBirthday(LocalDate firstDate, LocalDate secondDate) {
+        return "";
+    }
+
+}
